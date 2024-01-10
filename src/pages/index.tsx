9aae16e256741dc565b2b29292860182de43b1bb
Beto Piccoli <albertojuniorhc@gmail.com>
2024-01-09 16:50:51 -0800

    feat: font update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,16 +12,14 @@ export default function Home() {
         <title>Bruno Polengo</title>
         <meta name="description" content="Bruno Polengo WebSite" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-black">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-black font-mono text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          <h1 className="text-5xl font-extrabold text-white sm:text-[5rem]">
             Coming soon...
           </h1>
-          <p className="text-2xl text-cyan-50">
-            <a href="mailto:contato@brunopolengo.com.br">
-              contato@brunopolengo.com.br
-            </a>
-          </p>
+          <a href="mailto:contato@brunopolengo.com.br">
+            contato@brunopolengo.com.br
+          </a>
         </div>
       </main>
     </>
